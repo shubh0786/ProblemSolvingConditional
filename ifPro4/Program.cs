75f4800319567fbb4d4a8bf863872b6b73d75171
SHUBHAM SINGH <shubham.singh267@gmail.com>
2023-02-23 15:26:27 +1300

    Problem4

---
+++ ifPro4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Write a C# Sharp program to find whether a given year is a leap year or not.
+
+namespace ifPro4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
