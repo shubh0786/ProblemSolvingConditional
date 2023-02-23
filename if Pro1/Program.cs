489ffaca6aafb8acd4f6647acb9fff36b4e94e72
SHUBHAM SINGH <shubham.singh267@gmail.com>
2023-02-23 15:10:46 +1300

    Solved

--- if Pro1/Program.cs
+++ if Pro1/Program.cs
@@ -8,10 +8,34 @@ using System.Threading.Tasks;
 
 namespace if_Pro1
 {
-    internal class Program
+  public class Program
     {
+
+      
         static void Main(string[] args)
         {
+            int a, b;
+
+            Console.WriteLine("Please enter first number");
+
+            a = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Please enter second number");
+
+            b = int.Parse(Console.ReadLine());
+
+            if (a==b)
+            {
+
+                Console.WriteLine("Both numbers are equal: {0} {1}", a,b);
+            }
+            else
+            {
+
+                Console.WriteLine("Numers are not equal: first numeber{0} second number{1}", a,b);
+            }
+            Console.ReadLine();
         }
     }
 }
