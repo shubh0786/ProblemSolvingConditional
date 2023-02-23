f2bac8ef79bd1c73bb7877fc5e71c4f089e35ecf
SHUBHAM SINGH <shubham.singh267@gmail.com>
2023-02-23 15:24:26 +1300

    solved

--- ifPro3/Program.cs
+++ ifPro3/Program.cs
@@ -4,12 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+// Write a C# Sharp program to check whether a given number is positive or negative.
 namespace ifPro3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Please add your numer");
+
+            int a = int.Parse(Console.ReadLine());
+
+            if (a>=0)
+            {
+
+                Console.WriteLine("Number is positive {0}", a);
+
+            }
+            else
+            {
+
+                Console.WriteLine("Number is negative {0}", a);
+
+            }
+
+            Console.ReadLine();
         }
+
+
     }
 }
