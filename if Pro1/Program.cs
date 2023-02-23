dfb190d761e6d1bbbe3c958e6593cfdde520fa48
SHUBHAM SINGH <shubham.singh267@gmail.com>
2023-02-23 15:02:43 +1300

    Add project files.

---
+++ if Pro1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Write a C# Sharp program to accept two integers and check whether they are equal or not.
+
+namespace if_Pro1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
