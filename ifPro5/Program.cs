773abcb67b8ea0f46cf0f1d3859bfe064e904ff8
SHUBHAM SINGH <shubham.singh267@gmail.com>
2023-02-23 15:54:05 +1300

    solved

--- ifPro5/Program.cs
+++ ifPro5/Program.cs
@@ -4,12 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Write a C# Sharp program to read the age of a candidate and determine whether it is eligible for casting his/her own vote.
 namespace ifPro5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+
+
+            Console.Write("Please write your age");
+            int age = int.Parse(Console.ReadLine());
+
+
+
+            
+        
+
+            if (age >= 18)
+            {
+
+                Console.WriteLine("Your elgible to cast your vote");
+
+            }
+
+            else
+            {
+                Console.WriteLine("you are not eligible comeback after {0}",18-age);
+            }
+          Console.ReadLine();
+
         }
     }
 }
